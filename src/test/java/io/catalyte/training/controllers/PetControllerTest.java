10521e8fea22f721cbb4d285d47692cb12d235dc
Justin Williams <williams.jman@gmail.com>
2023-01-27 19:53:33 -0500

    Created the object mapper and MockMvc

--- src/test/java/io/catalyte/training/controllers/PetControllerTest.java
+++ src/test/java/io/catalyte/training/controllers/PetControllerTest.java
@@ -2,13 +2,20 @@ package io.catalyte.training.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @DirtiesContext
 @SpringBootTest
@@ -24,6 +31,19 @@ class PetControllerTest {
   ResultMatcher serverErrorStatus = MockMvcResultMatchers.status().isInternalServerError();
   ResultMatcher expectedType = MockMvcResultMatchers.content()
       .contentType(MediaType.APPLICATION_JSON);
+
+  ObjectMapper mapper = new ObjectMapper();
+
+  @Autowired
+  private WebApplicationContext wac;
+  private MockMvc mockMvc;
+
+  @BeforeEach
+  public void setUp() {
+    DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+    this.mockMvc = builder.build();
+
+  }
 
   @Test
   void getPet() {
